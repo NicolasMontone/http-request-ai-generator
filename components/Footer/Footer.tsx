1310a82558a0c73e2b4c0775e283ca0296bcdf8a
NicolasMontone <montonenicolas01@gmail.com>
2022-12-18 20:49:55 -0300

    fix(Footer): add github icon + link to repo and add padding.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { GithubIcon } from './GithubIcon'
 
 const Footer = () => (
-  <div className="flex justify-center text-xs gap-1">
+  <div className="flex justify-center items-end text-xs gap-1 pb-4">
     Made with love by Monto ❤️ |{' '}
     <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">
       Powered by Vercel
@@ -9,6 +10,13 @@ const Footer = () => (
     | and thanks to{' '}
     <a href="https://openai.com/" target="_blank" rel="noopener noreferrer">
       OpenAI
+    </a>
+    <a
+      href="https://github.com/NicolasMontone/http-request-ai-generator"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <GithubIcon />
     </a>
   </div>
 )
